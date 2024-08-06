e5ba0d18912f11dc3276ad50d33c8adc1c7571e1
Waris Fayiz <mohammadwaris456@gmail.com>
2024-08-06 20:38:05 +0330

    final solution

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,4 +1,49 @@
 const rockArray = ['Rock', 'Paper', 'Scissors']
+let computerScore = 0
+let humanScore = 0
+
+
 const getComputerChoice = () => {
-    return rockArray[Math.floor(Math.random() * 3)]
+    return rockArray[Math.floor(Math.random() * 3)].toLowerCase()
 }
+
+const getHumanChoice = () => {
+    const humanChoice =  prompt().toLocaleLowerCase()
+    return humanChoice
+}
+
+const playRound = (computerChoice, humanChoice) => {
+    if(computerChoice === humanChoice){
+        humanScore++
+        computerScore++
+        console.log(`tight: ${computerChoice} tight  ${humanChoice}\n Computer: ${computerScore}, Human: ${humanScore}` )
+    }else if(computerChoice == 'rock' && humanChoice == 'paper'){
+        humanScore++
+        console.log(`Human: ${humanChoice} wins ${computerChoice} \nHuman: ${humanScore}`)
+    }else if(computerChoice == 'rock' && humanChoice == 'scissors'){
+        computerScore++
+        console.log(`Computer: ${computerChoice} wins ${humanChoice} \nComputer: ${computerScore}`)
+    }else if(computerChoice == 'paper' && humanChoice == 'rock'){
+        computerScore++
+        console.log(`Computer: ${computerChoice} wins ${humanChoice} \nComputer: ${computerScore}`)
+    }else if(computerChoice == 'paper' && humanChoice == 'scissors'){
+        humanScore++
+        console.log(`Human: ${humanChoice} wins ${computerChoice} \nHuman: ${humanScore}`)
+    }else if(computerChoice == 'scissors' && humanChoice == 'rock'){
+        humanScore++
+        console.log(`Human: ${humanChoice} wins ${computerChoice} \nHuman: ${humanScore}`)
+    }else if(computerChoice == 'scissors' && humanChoice == 'paper'){
+        computerScore++
+        console.log(`Computer: ${computerChoice} wins ${humanChoice} \nComputer: ${computerScore}`)
+    }else {
+        console.log('Enter Rock, Paper, or Scissors')
+    }
+}
+
+const playGame = () => {
+    for(let i = 0; i < 5; i++){
+        playRound(getComputerChoice(), getHumanChoice())
+    }
+}
+
+playGame()
